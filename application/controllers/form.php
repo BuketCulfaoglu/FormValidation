3214624afd33543debabc27a3b8707ec5ea6d82c
Buket <buketculfaoglu@gmail.com>
2013-05-27 08:19:34 +0300

    Server Side Control

--- application/controllers/form.php
+++ application/controllers/form.php
@@ -8,15 +8,12 @@ class Form extends CI_Controller {
 		$this->load->library('form_validation');
 
 		$this->form_validation->set_rules('name', 'Name', 'required');
-		$this->form_validation->set_rules('email', 'Email', 'required');
-		$this->form_validation->set_rules('passport', 'Passport', 'required');
+		$this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+		$this->form_validation->set_rules('passport', 'Passport', 'required|min_length[8]|max_length[9]');
+		$this->form_validation->set_rules('mobile', 'Mobile', 'required|min_length[10]|max_length[14]');
+		$this->form_validation->set_rules('tckn', 'TCKN', 'required|min_length[11]|max_length[11]');
 		$this->form_validation->set_error_delimiters('<li>', '</li>');
-		/*
-		$this->form_validation->set_rules('username', 'Username', 'required|min_length[5]|max_length[12]|is_unique[users.username]');
-		$this->form_validation->set_rules('password', 'Password', 'required|matches[passconf]');
-		$this->form_validation->set_rules('passconf', 'Password Confirmation', 'required');
-		$this->form_validation->set_rules('email', 'Email', 'required|valid_email|is_unique[users.email]');
-		*/
+
 		
 		if ($this->form_validation->run() == FALSE) {
 			$data['title'] = "Hoşgeldiniz!";
