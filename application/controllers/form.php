17452e8d0075f4f0513b264f0c459d0cbb979b31
Buket <buketculfaoglu@gmail.com>
2013-05-27 08:10:37 +0300

    Server Side

--- application/controllers/form.php
+++ application/controllers/form.php
@@ -6,6 +6,17 @@ class Form extends CI_Controller {
 
 		$this->load->helper(array('form','url'));
 		$this->load->library('form_validation');
+
+		$this->form_validation->set_rules('name', 'Name', 'required');
+		$this->form_validation->set_rules('email', 'Email', 'required');
+		$this->form_validation->set_rules('passport', 'Passport', 'required');
+		$this->form_validation->set_error_delimiters('<li>', '</li>');
+		/*
+		$this->form_validation->set_rules('username', 'Username', 'required|min_length[5]|max_length[12]|is_unique[users.username]');
+		$this->form_validation->set_rules('password', 'Password', 'required|matches[passconf]');
+		$this->form_validation->set_rules('passconf', 'Password Confirmation', 'required');
+		$this->form_validation->set_rules('email', 'Email', 'required|valid_email|is_unique[users.email]');
+		*/
 		
 		if ($this->form_validation->run() == FALSE) {
 			$data['title'] = "Hoşgeldiniz!";
--- application/views/form.php
+++ application/views/form.php
@@ -3,14 +3,90 @@
 <head>
 	<meta charset="utf-8">
 	<title><?php echo $title; ?></title>
-<link rel="styleSheet" href="css/style.css" type="text/css" media="screen">
+<!--<link rel="styleSheet" href="css/style.css" type="text/css" media="screen">-->
+<style>
+body{
+   font-family:"Lucida Grande", "Lucida Sans Unicode", Verdana, Arial, Helvetica, sans-serif;
+   font-size:12px;
+ }
+ p, h1, form, button{border:0; margin:0; padding:0;}
+ .spacer{clear:both; height:1px;}
+ .someSpace{clear:both;height: 80px;}
+ /* ----------- My Form ----------- */
+ .form{
+   margin:0 auto;
+   width:600px;
+   padding:14px;
+ }
+ 
+   /* ----------- stylized ----------- */
+   #stylized{
+       border:solid 2px #b7ddf2;
+       background:#ebf4fb;
+   }
+   #stylized h1 {
+       font-size:14px;
+       font-weight:bold;
+       margin-bottom:8px;
+   }
+   #stylized p{
+       font-size:11px;
+       color:#666666;
+       margin-bottom:20px;
+       border-bottom:solid 1px #b7ddf2;
+       padding-bottom:10px;
+   }
+   #stylized label{
+       display:block;
+       font-weight:bold;
+       text-align:right;
+       width:240px;
+       float:left;
+   }
+   #stylized .small{
+       color:#666666;
+       display:block;
+       font-size:11px;
+       font-weight:normal;
+       text-align:right;
+       width:240px;
+   }
+   #stylized input{
+       float:left;
+       font-size:12px;
+       padding:4px 2px;
+       border:solid 1px #aacfe4;
+       width:300px;
+       margin:2px 0 20px 10px;
+   }
+   #stylized input.error{
+        background: #f8dbdb;
+        border-color: #e77776;
+   }
+   #stylized .error{
+        background: #f8dbdb;
+        color: #e77776;
+   }
+   #stylized button{
+       clear:both;
+       margin-left:250px;
+       width:125px;
+       height:31px;
+       background:#666666 url(img/button.png) no-repeat;
+       text-align:center;
+       line-height:31px;
+       color:#FFFFFF;
+       font-size:11px;
+       font-weight:bold;
+   }
+</style>
 <script type="text/javascript" src="js/jquery.js"></script>
 <script type="text/javascript" src="js/validate.js"></script>	
 </head>
 
 <body>
 
-<?php echo validation_errors(); ?>
+
 
 <div class="someSpace"></div>
 <div id="stylized" class="form">
@@ -18,6 +94,10 @@
 
 	<h1>Üyelik Formu</h1>
    	<p>isim, mail, pasapor numarası, cep telefonu, tckn</p>
+
+   	<ul class="error">
+   	<?php echo validation_errors(); ?>
+   </ul>
  
     <label>İsim
  		<span id="nameMsg" class="small">İsminizi giriniz</span>
